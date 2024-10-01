ec75a9c063361aa0eac8786b25a1f78f226a2cba
ItzMiracleOwO <miraclecutelemon@gmail.com>
2024-10-01 01:36:47 +0000

    add vercel analytics and insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -19,6 +22,8 @@ export default function RootLayout({
       <body className={`${inter.className} bg-black text-white`}>
         <Navbar />
         {children}
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   )

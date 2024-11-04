b67bdfc045719795a3175cc87ca089838410d3d5
YueYue <76854136+YueMiyuki@users.noreply.github.com>
2024-11-04 09:14:51 +0800

    L typo

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -16,7 +16,7 @@ export default function About() {
             At mdesk.tech, we're passionate about creating exceptional digital experiences. Our team of expert designers and developers work tirelessly to bring your vision to life.
           </p>
           <p className="text-xl mb-6">
-            Founded in 2022, we've quickly established ourselves as a leader in web design and hosting solutions. Our commitment to innovation, quality, and customer satisfaction sets us apart in the industry.
+            Founded in 2023, we've quickly established ourselves as a leader in web design and hosting solutions. Our commitment to innovation, quality, and customer satisfaction sets us apart in the industry.
           </p>
           <p className="text-xl">
             We believe in the power of the web to transform businesses and connect people. Let us help you make your mark in the digital world.
